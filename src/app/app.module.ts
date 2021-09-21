322f44ed6aef5e53bfa562fca5f7b48743ed3212
Nantawat Coombs <nantawat.coombs@quantiumfund.com>
2021-09-21 18:17:37 +0700

    For quantium, test markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarkedPipe } from './marked.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkedPipe
   ],
   imports: [
     BrowserModule,
